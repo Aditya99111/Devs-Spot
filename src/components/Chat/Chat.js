cf39274e07551272f10ed1f92cbb472684fba5d7
Abdul Suleiman <zaq09663@gmail.com>
2021-07-21 14:16:05 +0530

    set up project initial commit

---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Chat.css";
+
+function Chat() {
+  return (
+    <div className="container-fluid">
+      <div>i am the chat</div>
+    </div>
+  );
+}
+
+export default Chat;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="container-fluid">
+      <div>hi</div>
+    </div>
+  );
+}
+
+export default Header;
