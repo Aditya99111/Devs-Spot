fce67f6af9cd5a5fa463a91b89259aba3232d6b5
Abdul Suleiman <zaq09663@gmail.com>
2021-07-25 11:52:39 +0530

    Header: added some...

--- src/components/utils/index.js
+++ src/components/utils/index.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Header from './../Header/Header';
 
 function index(props) {
   return (
     <div>
       <div>Hello</div>
+      <div>Header</div>
     </div>
   );
 }
