a24d6b8d1cb300785aadbe845abf06a6377c85fe
Aditya <75898863+Aditya99111@users.noreply.github.com>
2021-07-28 15:55:48 +0530

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -15,15 +15,12 @@ function Header() {
             aria-controls="collapseExample"
           >
             <img className="BurgerMenu" src={BurgerMenu} alt="BurgerMenu" />
-<<<<<<< HEAD
 
-=======
->>>>>>> a1f9f61a4c4dfda78deba8bb0bd8057b8af31cc1
           </div>
           <div className="ml-auto LogoutLabel">Logout</div>
         </div>
       </div>
-<<<<<<< HEAD
+
           <div className="name">Devs-spot</div>
       <div className="collapse custom-collapse" id="collapseExample">
         <div className="card">
@@ -33,16 +30,7 @@ function Header() {
             <div className="ml-auto">Post</div>
             <div className="ml-auto">CratePost</div>
             <div className="ml-auto">Logout</div>
-=======
-      <div className="collapse custom-collapse" id="collapseExample">
-        <div className="card  custom-card-container">
-          <div className="card-body Nav-Items">
-            <div className="ml-auto Nav-Items">Profile</div>
-            <div className="ml-auto Nav-Items">Chat</div>
-            <div className="ml-auto Nav-Items">Post</div>
-            <div className="ml-auto Nav-Items">CratePost</div>
-            <div className="ml-auto Nav-Items">Logout</div>
->>>>>>> a1f9f61a4c4dfda78deba8bb0bd8057b8af31cc1
+
           </div>
         </div>
       </div>
