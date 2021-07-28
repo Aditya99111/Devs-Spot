e84c3d4a7570b357188af3e5631fdf752365282c
Aditya <75898863+Aditya99111@users.noreply.github.com>
2021-07-28 15:53:33 +0530

    Update CreatePost.js

--- src/components/CreatePost/CreatePost.js
+++ src/components/CreatePost/CreatePost.js
@@ -3,14 +3,14 @@ import "./CratePost.css";
 
 function CratePost() {
   return (
-<<<<<<< HEAD
+
     <div className="createpost_container">
     <h1>Create Posts</h1>
     <button>Create Posts</button>
-=======
+
     <div className="container-fluid">
       <div>i am the crate posts</div>
->>>>>>> a1f9f61a4c4dfda78deba8bb0bd8057b8af31cc1
+
     </div>
   );
 }
